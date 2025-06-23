df0e87f14a3801f6fd5b445ea7e7df23864e4163
torol2 <cephasz02@gmail.com>
2025-06-23 11:39:02 +0900

    sweetalert2 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+import Swal from 'sweetalert2'
+
 document.getElementById("danger-button").addEventListener("click", () => {
-  alert("클릭하지 마세요!");
+  Swal.fire({
+    icon: 'warning',
+    title: '클릭하지 마세요!',
+    text: '분명히 누르지 말라고 했잖아요 😠',
+    confirmButtonText: '죄송합니다...',
+    background: '#1b263b',
+    color: '#e0e1dd',
+    confirmButtonColor: '#778da9'
+  });
 });
